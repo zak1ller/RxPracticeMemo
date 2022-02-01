82463f16a69c5366145a21cd4d62cbf52ae67220
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-02-01 22:42:00 +0900

    🎉 프로젝트 생성!!

---
+++ RxPracticeMemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RxPracticeMemo
+//
+//  Created by Min-Su Kim on 2022/02/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
