8f11feea82531f0f33710878070258127fb15314
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-02-02 00:20:05 +0900

    🔧 메모리 저장소 구현

---
+++ RxPracticeMemo/Service/MemoStorageType.swift
@@ -0,0 +1,23 @@
+//
+//  MemoStorageTypew.swift
+//  RxPracticeMemo
+//
+//  Created by Min-Su Kim on 2022/02/02.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
