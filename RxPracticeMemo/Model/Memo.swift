479da87e3600d9bfbb7d5796bd871cef38a4eb78
Min-Su Kim <min-sukim@Min-Suui-MacBookPro-2.local>
2022-02-02 00:06:24 +0900

    🔧 Memo 모델 파일 추가

---
+++ RxPracticeMemo/Model/Memo.swift
@@ -0,0 +1,25 @@
+//
+//  Memo.swift
+//  RxPracticeMemo
+//
+//  Created by Min-Su Kim on 2022/02/02.
+//
+
+import Foundation
+
+struct Memo: Equatable {
+    var content: String
+    var insertDate: Date
+    var identity: String
+    
+    init(content: String, insertDate: Date = Date()) {
+        self.content = content
+        self.insertDate = insertDate
+        self.identity = "\(insertDate.timeIntervalSinceReferenceDate)"
+    }
+    
+    init(original: Memo, updatedContent: String) {
+        self = original
+        self.content = updatedContent
+    }
+}
